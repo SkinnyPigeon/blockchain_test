0aed18ec2e60d9b5ab5b8de69e7517cc5c855a7e
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 15:57:58 +0100

    starting to add in some neat stuff for working with the server

--- dev/api.js
+++ dev/api.js
@@ -6,7 +6,8 @@ app.get('/blockchain', function (req, res) {
 });
 
 app.post('/transaction', function(req, res) {
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
 app.get('/mine', function(req, res) {
