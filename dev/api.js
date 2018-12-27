8cebccfdddaad2e17cee4fea4b6b2144762b9354
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 15:48:24 +0100

    changed it a bit to give more explonitory end points

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,19 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
- res.send('Hello World')
-})
+app.get('/blockchain', function (req, res) {
 
-app.listen(3000)
+});
+
+app.post('/transaction', function(req, res) {
+
+});
+
+app.get('/mine', function(req, res) {
+
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000...'); 
+
+});
