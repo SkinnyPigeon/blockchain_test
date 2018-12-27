64b2b5ac8dddf71d3b0f15a183f3d92a1a177bfd
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 16:21:16 +0100

    updating the transaction endpoint to actually add it to the blockchain

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/blockchain', function (req, res) {
-
+app.get('/blockchain', function(req, res) {
+    res.send(bitcoin);
 });
 
 app.post('/transaction', function(req, res) {
-    console.log(req.body);
-    res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  app.post('/transaction', function(req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount,
+    req.body.sender, req.body.recipient) 
+      res.json({ note:`Transaction will be added in block
+      ${blockIndex}.`});
+  });
 });
 
 app.get('/mine', function(req, res) {
