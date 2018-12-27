7b9ff7ae819232ee824ae6e6292b71691b6a52bd
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 12:53:30 +0100

    function seems to be working fine

--- dev/test.js
+++ dev/test.js
@@ -1,12 +1,11 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-console.log(bitcoin);
 
-bitcoin.createNewBlock();
-console.log(bitcoin);
+bitcoin.createNewBlock(789457,'OIUOEDJETH8754DHKD','78SHNEG45DER56');
 
-bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
-bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
-bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
+bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
+
+bitcoin.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
+
 console.log(bitcoin);
