d647299f1571eedda1b54a8290a5598c5dc379d6
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 12:59:18 +0100

    looks good. Create transactions then the block to add them to it

--- dev/test.js
+++ dev/test.js
@@ -8,4 +8,12 @@ bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
 
 bitcoin.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
 
+bitcoin.createNewTransaction(50,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(200,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(300,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
+
+
+bitcoin.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
+
 console.log(bitcoin);
+console.log(bitcoin.chain[2]);
