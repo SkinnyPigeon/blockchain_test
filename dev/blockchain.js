88f6c4859f45a42762ddc9910cc0ac4b5e401933
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-27 15:26:08 +0100

    back

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -39,7 +39,18 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
   const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData)
+  const hash = sha256(dataAsString);
+
+  return hash;
 };
 
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4,) !== '0000') {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  };
+};
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,19 +1,14 @@
-const Blockchain = require('./blockchain');
-
-const bitcoin = new Blockchain();
-
-bitcoin.createNewBlock(789457,'OIUOEDJETH8754DHKD','78SHNEG45DER56');
-
-bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
-
-bitcoin.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
-
-bitcoin.createNewTransaction(50,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(200,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(300,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
+const Blockchain = require ('./blockchain'); 
+const bitcoin = new Blockchain (); 
 
 
-bitcoin.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
-
-console.log(bitcoin);
-console.log(bitcoin.chain[2]);
+const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'C4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',  
+    }  
+]
+const nonce = 100;
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
