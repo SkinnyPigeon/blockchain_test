65b6a6fa7a97ef0bea6fee4b1283fdf09ebdfd88
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-28 17:17:08 +0100

    added the address functin, about to write the endpoint

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -110,4 +110,25 @@ Blockchain.prototype.getTransaction = function(transactionId) {
   };
 };
 
+Blockchain.prototype.getAddressData = function(address) {
+  const addressTransactions = [];
+  this.chain.forEach(block => {
+    block.transactions.forEach(transaction => {
+      if(transaction.sender === address || transaction.recipient === address) {
+        addressTransactions.push(transaction);
+      };
+    });
+  });
+
+  let balance = 0;
+  addressTransactions.forEach(transaction => {
+    if (transaction.recipient === address) balance += transaction.amount;
+    else if (transaction.sender === address) balance -= transaction.amount;
+  });
+  return {
+    addressTransactions: addressTransactions,
+    addressBalance: balance
+  }
+};
+
 module.exports = Blockchain;
