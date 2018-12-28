a70e68cc2b55b47d690c9caefb716ce95dc85907
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-28 16:26:21 +0100

    lot of logic there but hopefully it's working

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -155,6 +155,37 @@ app.post('/register-nodes-bulk', function (req, res) {
   res.json({note: 'Bulk registration successful'})
 });
 
+app.get('/consensus', function(req, res) {
+  const requestPromises = [];
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/blockchain',
+      method: 'GET',
+      json: true
+    }
+    requestPromises.push(rp(requestOptions));
+  });
+  Promise.all(requestPromises).then(blockchains => {
+    const currentChainLength = bitcoin.chain.length;
+    let maxChainLength = currentChainLength;
+    let newLongestChain = null;
+    let newPendingTransactions = null;
+    blockchains.forEach(blockchain => {
+      if(blockchain.chain.length > maxChainLength) {
+        maxChainLength = blockchain.chain.length;
+        newLongestChain = blockchain.chain;
+        newPendingTransactions = blockchains.pendingTransactions;
+      }
+    })
+    if(!newLongestChain || (newLongestChain && !bitcoin.chainIsValid(newLongestChain))) {
+      res.json({
+        note: 'Current chain has not been replaced',
+        chain: bitcoin.chain
+      });
+    };
+  });         
+});
+
 app.listen(port, function() {
     console.log(`Listening on port ${port}...`);
 });
--- dev/test.js
+++ dev/test.js
@@ -1,26 +1,63 @@
-const Blockchain = require ('./blockchain'); 
-const bitcoin = new Blockchain (); 
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
 
+const bc1 = 
+  {
+  "chain": [
+  {
+  "index": 1,
+  "timestamp": 1546008687292,
+  "transactions": [],
+  "nonce": 100,
+  "hash": "0",
+  "previousBlockHash": "0"
+  },
+  {
+  "index": 2,
+  "timestamp": 1546008711882,
+  "transactions": [],
+  "nonce": 18140,
+  "hash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
+  "previousBlockHash": "0"
+  },
+  {
+  "index": 3,
+  "timestamp": 1546008766110,
+  "transactions": [
+  {
+  "amount": 12.5,
+  "sender": "00",
+  "recipient": "0dc336d00ab011e98f9427db2476b05d",
+  "transactionId": "1c6f2a400ab011e98f9427db2476b05d"
+  },
+  {
+  "amount": 600,
+  "sender": "Z9PNS89GFN9RASNF89",
+  "recipient": "ADSDF8DQZSD9FNASD",
+  "transactionId": "2fb84c800ab011e98f9427db2476b05d"
+  },
+  {
+  "amount": 400,
+  "sender": "Z9GNS89GFN9RASNF89",
+  "recipient": "ADSDFTDQZSD9FNASD",
+  "transactionId": "343019000ab011e98f9427db2476b05d"
+  }
+  ],
+  "nonce": 65957,
+  "hash": "00001bdf4b39b44f48be7cc3fa20f062983dc43e88d43292114b0f6a0e7c4f9d",
+  "previousBlockHash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100"
+  }
+  ],
+  "pendingTransactions": [
+  {
+  "amount": 12.5,
+  "sender": "00",
+  "recipient": "0dc336d00ab011e98f9427db2476b05d",
+  "transactionId": "3cbe31100ab011e98f9427db2476b05d"
+  }
+  ],
+  "currentNodeUrl": "http://localhost:3001",
+  "networkNodes": []
+}
 
-const previousBlockHash = 'OINAISDFN09N09ASDNF90N90ASNDF';
-const currentBlockData = [
-    {
-        amount: 101,
-        sender: 'N90ANS90N90ANSDFN',
-        recipient: '90NA90SNDF90ANSDF09N'  
-    },
-    {
-        amount: 30,
-        sender: '09ANS09DFNA8SDNF',
-        recipient: 'UIANIUDFUIABSDUIFB'  
-    },
-    {
-        amount: 200,
-        sender: '89ANS89DFN98ASNF89',
-        recipient: 'AUSDF89ANSD9FNASD' 
-    }      
-]
-const nonce = 82677;
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log('VALID:' , bitcoin.chainIsValid(bc1.chain));
