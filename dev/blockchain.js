214558e555743db99b119280385fc8c0b570fcd1
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-12-28 16:58:44 +0100

    built the function for searching transactions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -93,4 +93,20 @@ Blockchain.prototype.getBlock = function(blockHash) {
   return correctBlock;
 };
 
+Blockchain.prototype.getTransaction = function(transactionId) { 
+  let correctTransaction = null;
+  this.chain.forEach(block => {
+    block.transactions.forEach(transaction => {
+      if(transaction.transactionId === transactionId) {
+        correctTransaction = transaction;
+        correctBlock = block
+      };
+    })
+  });
+  return {
+    transaction: correctTransaction,
+    block: correctBlock 
+  };
+});
+
 module.exports = Blockchain;
